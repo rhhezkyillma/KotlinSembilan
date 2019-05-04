a6345cf227d2b2e1b5c193db72d5c658160404df
rhhezkyillma <rhhezkyillma@gmail.com>
2019-05-04 15:59:22 +0700

    Update Full Cost Type, Other Ask Type, People Wanted Type For upload image

--- app/src/main/java/com/example/reezkyillma/projectandroidlatihanfinal/OtherAskType.kt
+++ app/src/main/java/com/example/reezkyillma/projectandroidlatihanfinal/OtherAskType.kt
@@ -1,12 +1,141 @@
 package com.example.reezkyillma.projectandroidlatihanfinal
 
+import android.Manifest
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.annotation.RequiresApi
+import android.support.v4.content.ContextCompat
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.io.IOException
+import java.util.*
 
 class OtherAskType : AppCompatActivity() {
+
+    val PICK_IMAGE_REQUEST = 71
+    val PERMISSION_REQUEST_CODE = 1001
+    var value = 0.0
+    lateinit var image : ImageView
+    lateinit var post : Button
+    lateinit var filepath : Uri
+    lateinit var storage : FirebaseStorage
+    lateinit var storageReference: StorageReference
+
+    @RequiresApi(Build.VERSION_CODES.M)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other_ask_type)
+
+        image = findViewById(R.id.image)
+        post = findViewById(R.id.post_otherask)
+
+        storage = FirebaseStorage.getInstance()
+
+        storageReference = storage.reference
+
+        post.setOnClickListener {
+            uploadFile()
+        }
+
+        image.setOnClickListener {
+            when {
+                (Build.VERSION.SDK_INT >= Build
+                    .VERSION_CODES.M) -> {
+                    if (ContextCompat.checkSelfPermission(
+                            this@OtherAskType, Manifest
+                                .permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(arrayOf(
+                            Manifest.permission
+                                .READ_EXTERNAL_STORAGE),
+                            PERMISSION_REQUEST_CODE)
+                    }else{
+                        chooseImage()
+                    }
+                }
+                else -> chooseImage()
+            }
+        }
+    }
+
+    private fun chooseImage() {
+        val intent = Intent().apply {
+            type = "image/*"
+            action = Intent.ACTION_GET_CONTENT
+        }
+        startActivityForResult(
+            Intent.createChooser(intent,
+                "Select Picture"),
+            PICK_IMAGE_REQUEST)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0]
+                    == PackageManager.PERMISSION_GRANTED)
+                    Toast.makeText(this@OtherAskType,
+                        "Oops! Permission Denied!!"
+                        , Toast.LENGTH_SHORT).show()
+                else
+                    chooseImage()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && data != null && data.data !=null) {
+            filepath = data.data
+
+            try {
+                var bitmap : Bitmap = MediaStore.Images.Media.getBitmap(contentResolver,filepath)
+                image.setImageBitmap(bitmap)
+            }catch (e : IOException){
+                e.printStackTrace()
+            }
+        }
+
+    }
+
+    private fun uploadFile() {
+        val progress = ProgressDialog(this)
+            .apply {
+                setTitle("Uploading Picture....")
+                setCancelable(false)
+                setCanceledOnTouchOutside(false)
+                show()
+            }
+        var ref : StorageReference = storageReference.child("OtherAskType/"
+                + UUID.randomUUID().toString())
+        ref.putFile(filepath)
+            .addOnSuccessListener {
+                    taskSnapshot -> progress.dismiss()
+                Toast.makeText(this@OtherAskType, "Uploaded", Toast.LENGTH_SHORT).show();
+            }.addOnFailureListener{
+                    e -> progress.dismiss()
+                Toast.makeText(this@OtherAskType, "Failed" + e.message, Toast.LENGTH_SHORT).show();
+            }.addOnProgressListener {
+                    taskSnapshot ->
+                value = (100.0 * taskSnapshot
+                    .getBytesTransferred()/ taskSnapshot
+                    .getTotalByteCount())
+
+                progress.setMessage("Uploaded.."
+                        + value.toInt() + "%")
+            }
     }
 }
--- app/src/main/java/com/example/reezkyillma/projectandroidlatihanfinal/PeopleWantedType.kt
+++ app/src/main/java/com/example/reezkyillma/projectandroidlatihanfinal/PeopleWantedType.kt
@@ -1,12 +1,146 @@
 package com.example.reezkyillma.projectandroidlatihanfinal
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.annotation.TargetApi
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.annotation.RequiresApi
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.activity_people_wanted_type.*
+import java.io.IOException
+import java.util.*
 
 class PeopleWantedType : AppCompatActivity() {
+
+    val PICK_IMAGE_REQUEST = 71
+    val PERMISSION_REQUEST_CODE = 1001
+    var value = 0.0
+    lateinit var image : ImageView
+    lateinit var post : Button
+    lateinit var filepath : Uri
+    lateinit var storage : FirebaseStorage
+    lateinit var storageReference: StorageReference
+
+    @RequiresApi(Build.VERSION_CODES.M)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_people_wanted_type)
+
+        image = findViewById(R.id.image)
+        post = findViewById(R.id.post_peoplewanted)
+
+        storage = FirebaseStorage.getInstance()
+
+        storageReference = storage.reference
+
+        post.setOnClickListener {
+            uploadFile()
+        }
+
+        image.setOnClickListener {
+            when {
+                (Build.VERSION.SDK_INT >= Build
+                    .VERSION_CODES.M) -> {
+                    if (ContextCompat.checkSelfPermission(
+                            this@PeopleWantedType, Manifest
+                                .permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissions(arrayOf(
+                            Manifest.permission
+                                .READ_EXTERNAL_STORAGE),
+                            PERMISSION_REQUEST_CODE)
+                    }else{
+                        chooseImage()
+                    }
+                }
+                else -> chooseImage()
+            }
+        }
+    }
+
+    private fun chooseImage() {
+        val intent = Intent().apply {
+            type = "image/*"
+            action = Intent.ACTION_GET_CONTENT
+        }
+        startActivityForResult(
+            Intent.createChooser(intent,
+                "Select Picture"),
+            PICK_IMAGE_REQUEST)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0]
+                    == PackageManager.PERMISSION_GRANTED)
+                    Toast.makeText(this@PeopleWantedType,
+                        "Oops! Permission Denied!!"
+                        , Toast.LENGTH_SHORT).show()
+                else
+                    chooseImage()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && data != null && data.data !=null) {
+            filepath = data.data
+
+            try {
+                var bitmap : Bitmap = MediaStore.Images.Media.getBitmap(contentResolver,filepath)
+                image.setImageBitmap(bitmap)
+            }catch (e : IOException){
+                e.printStackTrace()
+            }
+        }
+
+    }
+
+    private fun uploadFile() {
+        val progress = ProgressDialog(this)
+            .apply {
+                setTitle("Uploading Picture....")
+                setCancelable(false)
+                setCanceledOnTouchOutside(false)
+                show()
+            }
+        var ref : StorageReference = storageReference.child("PeopleWantedType/"
+                + UUID.randomUUID().toString())
+        ref.putFile(filepath)
+            .addOnSuccessListener {
+                    taskSnapshot -> progress.dismiss()
+                Toast.makeText(this@PeopleWantedType, "Uploaded", Toast.LENGTH_SHORT).show();
+            }.addOnFailureListener{
+                    e -> progress.dismiss()
+                Toast.makeText(this@PeopleWantedType, "Failed" + e.message, Toast.LENGTH_SHORT).show();
+            }.addOnProgressListener {
+                    taskSnapshot ->
+                value = (100.0 * taskSnapshot
+                    .getBytesTransferred()/ taskSnapshot
+                    .getTotalByteCount())
+
+                progress.setMessage("Uploaded.."
+                        + value.toInt() + "%")
+            }
     }
 }
